4311cd7f31b15540635c71caeae683cfbb4b818e
nguyenduykhang-y <duykhang148w@gmail.com>
2021-10-16 15:37:14 +0700

    update

---
+++ app/src/main/java/com/example/fpt_app/CartActivity.java
@@ -0,0 +1,49 @@
+package com.example.fpt_app;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navi);
+
+        bottomNavigationView.setSelectedItemId(R.id.cart);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.shop:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.cart:
+                        return true;
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), ProductActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.noti:
+                        startActivity(new Intent(getApplicationContext(), NotificaitonActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.user:
+                        startActivity(new Intent(getApplicationContext(), ThontinActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/fpt_app/ThontinActivity.java
+++ app/src/main/java/com/example/fpt_app/ThontinActivity.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import com.example.fpt_app.Models.AccessToken;
 import com.example.fpt_app.Models.AccessTokenManager;
@@ -19,32 +23,51 @@ public class ThontinActivity extends AppCompatActivity {
     private String BASE_URL = "http://10.0.2.2:8081/";
     private AccessTokenManager tokenManager;
     Button btnout;
+    private Switch aSwitch;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            setTheme(R.style.Theme_Dark);
+        }else {
+            setTheme(R.style.Theme_Light );
+        }
+
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_thontin);
         btnout = findViewById(R.id.btnlogout);
+        aSwitch = findViewById(R.id.mode);
+        textView = findViewById(R.id.tv);
         setTitle("Info");
         tokenManager = AccessTokenManager.getInstance(getSharedPreferences("prefs", MODE_PRIVATE));
         AccessToken token = tokenManager.getToken();
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navi);
 
-        bottomNavigationView.setSelectedItemId(R.id.noti);
+        bottomNavigationView.setSelectedItemId(R.id.user);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId())
                 {
-                    case R.id.user:
-                        startActivity(new Intent(getApplicationContext(), ProductActivity.class));
-                        overridePendingTransition(0, 0);
-                        return true;
-                    case R.id.home:
+                    case R.id.shop:
                         startActivity(new Intent(getApplicationContext(), MainActivity.class));
                         overridePendingTransition(0, 0);
                         return true;
+                    case R.id.cart:
+                        startActivity(new Intent(getApplicationContext(), CartActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(), ProductActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
                     case R.id.noti:
+                        startActivity(new Intent(getApplicationContext(), NotificaitonActivity.class));
+                        overridePendingTransition(0, 0);
+                        return true;
+                    case R.id.user:
+
                         return true;
                 }
                 return false;
@@ -64,8 +87,34 @@ public class ThontinActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES){
+            aSwitch.setChecked(true);
+        }
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    textView.setText("Dark");
+                    reset();
+
+                }else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    textView.setText("Light");
+                    reset();
+
+                }
+            }
+        });
     }
 
+    private void reset() {
+        Intent i = new Intent(getApplicationContext(), ThontinActivity.class);
+        startActivity(i);
+        finish();
+    }
 
 
 }
