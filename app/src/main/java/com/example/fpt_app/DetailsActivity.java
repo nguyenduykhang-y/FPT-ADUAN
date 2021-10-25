5dc17fbe61e430ccac19780ddfe18436ca7e18fb
nguyenduykhang-y <duykhang148w@gmail.com>
2021-10-25 16:22:39 +0700

    update

--- app/src/main/java/com/example/fpt_app/DetailsActivity.java
+++ app/src/main/java/com/example/fpt_app/DetailsActivity.java
@@ -3,11 +3,6 @@ package com.example.fpt_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-<<<<<<< HEAD
-import android.widget.Button;
-import android.widget.ImageView;
-=======
->>>>>>> 9b3a8e1 (update)
 import android.widget.TextView;
 
 import com.example.fpt_app.Models.ListSP;
@@ -23,22 +18,10 @@ public class DetailsActivity extends AppCompatActivity {
         if (bundle == null ){
             return;
         }
-<<<<<<< HEAD
-        ListSP listSP = (ListSP) bundle.get("chitiet");
-        ImageView img = findViewById(R.id.imgdetail);
-        TextView tv = findViewById(R.id.tvTesst);
-        Button btn = findViewById(R.id.mua);
-        TextView tvGia = findViewById(R.id.tvGia);
-        img.setImageResource(listSP.getImg());
-        tv.setText(listSP.getName());
-        tvGia.setText(listSP.getGia());
-        btn.setText(listSP.getGia());
-=======
         ListSP listSP = (ListSP) bundle.get("ọject");
         TextView tv = findViewById(R.id.tvTesst);
         TextView tvGia = findViewById(R.id.tvGia);
         tv.setText(listSP.getName());
         tvGia.setText(listSP.getGia());
->>>>>>> 9b3a8e1 (update)
     }
 }
