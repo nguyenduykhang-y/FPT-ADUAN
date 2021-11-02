3b0fdba79af861a3cb8d16febbd7fa35d451d281
dinhthai <dinhthai62001@gmail.com>
2021-11-02 16:02:20 +0700

    thaidz

---
+++ app/src/main/java/com/example/fpt_app/welcome_screen.java
@@ -0,0 +1,28 @@
+package com.example.fpt_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+
+public class welcome_screen extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                finish();
+            }
+        }, 2000);
+
+
+    }
+}
