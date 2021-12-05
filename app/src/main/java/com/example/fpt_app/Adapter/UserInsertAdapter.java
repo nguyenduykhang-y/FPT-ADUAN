1bc646351d20dade1c8ade8e501fc734b0269bda
An Oliver <uyted023@gmail.com>
2021-12-05 13:24:17 +0700

    456

--- app/src/main/java/com/example/fpt_app/Adapter/UserInsertAdapter.java
+++ app/src/main/java/com/example/fpt_app/Adapter/UserInsertAdapter.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -29,7 +30,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class UserInsertAdapter extends RecyclerView.Adapter<UserInsertAdapter.UserInsertViewHolder> {
+public class UserInsertAdapter extends BaseAdapter {
     private static String BASE_URL = "http://10.0.2.2:8081/";
     private List<Product> data;
     private Context context;
@@ -40,84 +41,52 @@ public class UserInsertAdapter extends RecyclerView.Adapter<UserInsertAdapter.Us
         this.context = context;
     }
 
-
-    @NonNull
     @Override
-    public UserInsertAdapter.UserInsertViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_user_insert, parent, false);
-        return new UserInsertViewHolder(v);
+    public int getCount() {
+        return data.size();
     }
 
     @Override
-    public void onBindViewHolder(@NonNull UserInsertAdapter.UserInsertViewHolder holder, @SuppressLint("RecyclerView") int position) {
-        Product product = data.get(position);
-        if (product == null) {
-            return;
+    public Object getItem(int i) {
+        return data.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View View = view;
+        if (view == null){
+            view = View.inflate(viewGroup.getContext(), R.layout.item_user_insert, null);
+            TextView tvUserProductName = (TextView) view.findViewById(R.id.tvUserProductName);
+            TextView tvUserProductPrice = (TextView) view.findViewById(R.id.tvUserProductPrice);
+
+
+            ImageView userProImg = (ImageView) view.findViewById(R.id.ivUserProductImage);
+            UserInsertViewHolder holder = new UserInsertViewHolder(tvUserProductName, tvUserProductPrice, userProImg);
+            view.setTag(holder);
         }
-        DecimalFormat decimalFormat = new DecimalFormat("###,###,###.#");
-        Glide.with(context).load(product.getImage_url())
+        UserInsertViewHolder holder = (UserInsertViewHolder) view.getTag();
+        Product p = (Product) getItem(i);
+        holder.tvUserProductName.setText(p.getName());
+        holder.tvUserProductPrice.setText(String.valueOf(p.getPrice()) + " VNĐ");
+        Glide.with(context).load(p.getImage_url())
                 .into(holder.userProImg);
-        holder.tvUserProductName.setText(product.getName());
-        holder.tvUserProductPrice.setText(decimalFormat.format(product.getPrice()) + " VND");
 
-        holder.mCardView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent in = new Intent(context, ProductFormActivity.class);
-                context.startActivity(in);
-
-            }
-        });
-        holder.userDelImg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-
-
-                IRetrofitService service = new RetrofitBuilder().createService(IRetrofitService.class, BASE_URL);
-
-                service.productDelete(data.get(position)).enqueue(new Callback<ResponseModel>() {
-                    @Override
-                    public void onResponse(Call<ResponseModel> call, Response<ResponseModel> response) {
-
-                    }
-
-                    @Override
-                    public void onFailure(Call<ResponseModel> call, Throwable t) {
-                        Log.d("fail",""+t.getMessage() );
-                    }
-                });
-                data.remove(position);
-                notifyItemRemoved(position);
-                notifyItemRangeChanged(position, data.size());
-                notifyDataSetChanged();
-            }
-        });
-
+        return view;
     }
 
-    @Override
-    public int getItemCount() {
-        if(data !=null ){
-            return data.size();
-        }
+    private static class UserInsertViewHolder{
+        final TextView tvUserProductName, tvUserProductPrice;
+        final ImageView userProImg;
 
-        return 0;
-    }
-
-    public class UserInsertViewHolder extends RecyclerView.ViewHolder {
-        private ImageView userProImg, userDelImg, userBackIcon;
-        private TextView tvUserProductName, tvUserProductPrice;
-        private CardView mCardView;
-
-        public UserInsertViewHolder(@NonNull View itemView) {
-            super(itemView);
-            userProImg = itemView.findViewById(R.id.ivUserProductImage);
-            tvUserProductName = itemView.findViewById(R.id.tvUserProductName);
-            tvUserProductPrice = itemView.findViewById(R.id.tvUserProductPrice);
-            mCardView = itemView.findViewById(R.id.user_insert_item);
-            userDelImg = itemView.findViewById(R.id.ivUserProductDelete);
+        public UserInsertViewHolder(TextView tvUserProductName, TextView tvUserProductPrice, ImageView userProImg) {
+            this.tvUserProductName = tvUserProductName;
+            this.tvUserProductPrice = tvUserProductPrice;
+            this.userProImg = userProImg;
         }
     }
 }
