e1e4bf0a81d8b8ef5c6a731566ce82e62252ed33
NamNN12 <namnnmb12@gmail.com>
2021-12-12 19:14:29 +0700

    comit

--- app/src/main/java/com/example/fpt_app/CartActivity.java
+++ app/src/main/java/com/example/fpt_app/CartActivity.java
@@ -41,6 +41,8 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -56,11 +58,13 @@ public class CartActivity extends AppCompatActivity {
     private AccessTokenManager tokenManager;
     EditText quantity;
     Button btnThanhToan;
-    String  name;
+    String name;
     int id;
-    int oderctId = 1;
-    int userId ;
-    String phone,nameuser;
+    Button btnCancle ,btnOke,btnDate;
+    EditText edtQuantity,edtAddress,edtDate;
+    int userId;
+    String phone, nameuser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,65 +72,135 @@ public class CartActivity extends AppCompatActivity {
         //ánh xạ
         mRecycle = findViewById(R.id.lvCart);
         txtGiaTien = findViewById(R.id.TvGiaTien);
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,1);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 1);
         mRecycle.setLayoutManager(gridLayoutManager);
-        btnThanhToan= findViewById(R.id.btnThanhtoan);
+        btnThanhToan = findViewById(R.id.btnThanhtoan);
         tokenManager = AccessTokenManager.getInstance(getSharedPreferences("prefs", MODE_PRIVATE));
-
-
 
         IRetrofitService service = new RetrofitBuilder()
                 .createService(IRetrofitService.class, BASE_URL);
 
         service.CartGetALL().enqueue(getALLCart);
-
-
-
+        service.Profile().enqueue(getProfile);
 
 
         btnThanhToan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                OderCT oderCT = new OderCT();
 
-                List<Cart> cartList = ((CartAdapter) mRecycle.getAdapter()).getData();
-                for (Cart cart : cartList){
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(v.getRootView().getContext());
+                View dialogView = LayoutInflater.from(v.getRootView().getContext()).inflate(R.layout.custom_dialog_ct, null);
+                alertDialog.setView(dialogView);
+                alertDialog.setCancelable(true);
+                btnOke = dialogView.findViewById(R.id.btnOke);
+                btnCancle= dialogView.findViewById(R.id.btnCancle);
+                edtQuantity =dialogView.findViewById(R.id.edtQuantity);
+                edtAddress = dialogView.findViewById(R.id.edtAddress);
+                btnDate = dialogView.findViewById(R.id.btnDate);
+                edtDate = dialogView.findViewById(R.id.edtDate);
+                AlertDialog dialog = alertDialog.create();
+                dialog.show();
 
-                    oderCT.setProductId(cart.getIdProduct());
-                    oderCT.setQuantity(cart.getQuantity());
-                    oderCT.setPrice(cart.getPrice());
-                    oderCT.setDate(cart.getDate());
-                    oderCT.setAddress(cart.getAddress());
-                }
+                btnDate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Date today = new Date();
+                        Calendar cal = Calendar.getInstance();
+                        cal.setTime(today);
 
+                        final int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+                        final int months = cal.get(Calendar.MONTH);
+                        final int years = cal.get(Calendar.YEAR);
+                        final Calendar calendar = Calendar.getInstance();
+                        int date = calendar.get(Calendar.DAY_OF_MONTH);
+                        int month = calendar.get(Calendar.MONTH);
+                        int year = calendar.get(Calendar.YEAR);
 
+                        DatePickerDialog datePickerDialog = new DatePickerDialog(CartActivity.this,new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                                calendar.set(i,i1,i2);
+                                edtDate.setText(simpleDateFormat.format(calendar.getTime()));
+                            }
+                        },years,months,dayOfWeek);
+                        datePickerDialog.show();
+                    }
 
-//                service.insertOderCT(oderCT).enqueue(inserOderCT);
+                });
+                btnOke.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Map<Cart, Integer> cartMap = ((CartAdapter) mRecycle.getAdapter()).getCartMap();
+                        String orderId = UUID.randomUUID().toString();
+                        for (Map.Entry<Cart, Integer> e : cartMap.entrySet()) {
+                            Log.d("TAG", "onClick: " + orderId);
+                            OderCT oderCT = new OderCT();
+                            oderCT.setOderctId(orderId);
+                            oderCT.setUserId(userId);
+                            oderCT.setProductId(e.getKey().getIdProduct());
+                            oderCT.setQuantity(e.getValue());
+                            oderCT.setPrice(e.getKey().getPrice());
+                            oderCT.setDate(edtDate.getText().toString());
+                            oderCT.setAddress(edtAddress.getText().toString());
+                            service.insertOderCT(oderCT).enqueue(inserOderCT);
+                        }
+                        service.cart_delete_all().enqueue(delete_all_cart);
+                    dialog.cancel();
+                     Intent i = new Intent(CartActivity.this, MainActivity.class);
+                     startActivity(i);
+                    }
+                });
+                btnCancle.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.cancel();
+                    }
+                });
 
 
             }
         });
 
 
-
     }
 
 
-
-    private Context getCt(){
+    private Context getCt() {
         return this;
     }
+
     Callback<ResponseModel> inserOderCT = new Callback<ResponseModel>() {
         @Override
         public void onResponse(Call<ResponseModel> call, Response<ResponseModel> response) {
-            if (response.isSuccessful()){
+            if (response.isSuccessful()) {
                 ResponseModel model = response.body();
-                if(model.getStatus()){
+                if (model.getStatus()) {
                     Toast.makeText(CartActivity.this, "Đã  mua hàng", Toast.LENGTH_SHORT).show();
                 } else {
                     Log.e(">>>>>insertCB getStatus failed", "insert failed");
                 }
-            } else{
+            } else {
+                Log.e(">>>>>insertCB onResponse", response.message());
+            }
+        }
+
+        @Override
+        public void onFailure(Call<ResponseModel> call, Throwable t) {
+            Log.e(">>>>>insertCB onFailure", t.getMessage());
+        }
+    };
+    Callback<ResponseModel> delete_all_cart = new Callback<ResponseModel>() {
+        @Override
+        public void onResponse(Call<ResponseModel> call, Response<ResponseModel> response) {
+            if (response.isSuccessful()) {
+                ResponseModel model = response.body();
+                if (model.getStatus()) {
+                    Toast.makeText(CartActivity.this, "dax xoa", Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.e(">>>>>insertCB getStatus failed", "insert failed");
+                }
+            } else {
                 Log.e(">>>>>insertCB onResponse", response.message());
             }
         }
@@ -139,20 +213,21 @@ public class CartActivity extends AppCompatActivity {
     Callback<List<Cart>> getALLCart = new Callback<List<Cart>>() {
         @Override
         public void onResponse(Call<List<Cart>> call, Response<List<Cart>> response) {
-            if (response.isSuccessful()){
-                if (data.size() == 0){
+            if (response.isSuccessful()) {
+                if (data.size() == 0) {
                     data = response.body();
                     adapter = new CartAdapter(getCt(), data);
 
                     double sum = 0;
-                    List<Cart> carts;
-                    carts = data;
-                        for (Cart c : carts){
+                    List<Cart> carts = data;
+                    if (carts != null) {
+                        for (Cart c : carts) {
 //                        double so = (double) (c.getPrice()* Double.parseDouble(getIntent().getStringExtra("soluong")));
-                            sum += c.getPrice()*c.getQuantity();
+                            sum += c.getPrice();
                         }
-                        DecimalFormat decimalFormat = new DecimalFormat("###,###,###.#");
-                        txtGiaTien.setText(decimalFormat.format(sum)+" VNĐ");
+                    }
+                    DecimalFormat decimalFormat = new DecimalFormat("###,###,###.#");
+                    txtGiaTien.setText(decimalFormat.format(sum) + " VNĐ");
 
 
                     mRecycle.setAdapter(adapter);
@@ -177,8 +252,8 @@ public class CartActivity extends AppCompatActivity {
     Callback<User> getProfile = new Callback<User>() {
         @Override
         public void onResponse(Call<User> call, Response<User> response) {
-            if (response.isSuccessful()){
-                User u =  new User();
+            if (response.isSuccessful()) {
+                User u = new User();
                 u = response.body();
                 userId = u.getUserId();
                 nameuser = u.getName();
